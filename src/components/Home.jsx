b232d49fc90648500c635c75efabc964eccd9113
momosetti <zinasetti@gmail.com>
2021-02-15 01:33:43 +0100

    Create home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+
+export default function Home() {
+  const [countries, setCountries] = useState([]);
+  const [filterQuery, setFilterQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const filterChanged = (event) => setFilterQuery(event.target.value);
+  const searchInput = (event) => setSearchQuery(event.target.value);
+  let filtredArr = countries
+    .filter((el) => el.region.toLowerCase().includes(filterQuery.toLowerCase()))
+    .filter((el) => el.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <main>
+      <div className="container">
+        <div className="search-filter">
+          <input
+            onChange={searchInput}
+            type="text"
+            id="search"
+            placeholder="Search for a country..."
+          />
+          <select onChange={filterChanged} name="filter" id="region">
+            <option value="">Filter By Region</option>
+            <option value="africa">Africa</option>
+            <option value="america">America</option>
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+            <option value="oceania">Oceania</option>
+          </select>
+        </div>
+      </div>
+      <div className="container">
+        <div className="cards">
+          {" "}
+          {filtredArr.map((el) => (
+            <Card data={el} />
+          ))}{" "}
+        </div>
+      </div>
+    </main>
+  );
+}
