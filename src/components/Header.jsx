7d43b5535254763e214b483d3dce006c3560bf02
momosetti <zinasetti@gmail.com>
2021-02-15 01:33:04 +0100

    Implement toogle theme swither

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [isDark, setTheme] = useState(localStorage.getItem("isDark") || false);
+  const toogleSwitch = () => {
+    try {
+      localStorage.setItem("isDark", "true");
+      setTheme(!isDark);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <header>
       <nav>
         <div class="container">
           <h1>Where in the world</h1>
           <div id="Dark-mode-toggle" class="darkmode">
-            <button>
-              <i class="ri-moon-line" /> Dark Mode
+            <button onClick={toogleSwitch}>
+              <i class={isDark ? "ri-sun-line" : "ri-moon-line"}></i>
+              {isDark ? "Light " : "Dark "}
+              Mode
             </button>
           </div>
         </div>
